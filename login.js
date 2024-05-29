438092aababd3ee1a0210e021efb1e290e38b65f
Hanniamon <127470424+Hanniamon@users.noreply.github.com>
2024-05-28 22:12:36 -0600

    Prueba

---
+++ login.js
@@ -0,0 +1,23 @@
+LoginPage();
+
+function LoginPage () {
+
+    return (
+
+        <div className = «loginPage»>
+
+<h1 className = «loginPage-title»>Log in to Twitter</h1>
+
+    <form>
+
+        <input type = «text» name= «username»></input>
+
+    <input type= «password» name= «password»/>
+
+</form>
+
+</div>
+
+);
+
+}
